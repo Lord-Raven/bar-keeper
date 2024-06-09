5fe12cfb8c1bcbaa8cf6bacade6f9f1ba0587cce
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-09 11:58:58 -0400

    Trying to fix pop-up.

--- src/ImageWithPopup.tsx
+++ src/ImageWithPopup.tsx
@@ -24,28 +24,29 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
     };
 
     return (
+        <div>
             <img
                 src={src}
                 alt={alt}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 style={style ?? { margin: '0 5px' }}
+            />
+            <Popover
+                id={popupHeader}
+                open={showPopup}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
             >
-                <Popover
-                    id={popupHeader}
-                    open={showPopup}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                >
-                    <b>{popupHeader}</b> - {popupBody}
-                </Popover>
-            </img>
+                <b>{popupHeader}</b> - {popupBody}
+            </Popover>
+        </div>
     );
 };
 
