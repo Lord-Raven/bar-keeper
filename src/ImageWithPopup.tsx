f53dd0609908d2a4a29139f774ac9db66a2b794b
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-14 09:17:57 -0400

    Messing with layout.

--- src/ImageWithPopup.tsx
+++ src/ImageWithPopup.tsx
@@ -54,7 +54,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
                 onClose={handlePopoverClose}
                 disableRestoreFocus
             >
-                <Box component="section" sx={{p: 2, border: '1px dashed grey'}}>
+                <Box component="section" sx={{height: '100%', p: 2, border: '1px dashed grey'}}>
                     {popupSrc && (
                         <div style={{height: '100%', float: 'left'}}>
                             <img src={popupSrc} style={{height: '100%', width: 'auto', verticalAlign: 'center'}} alt={alt}/>
