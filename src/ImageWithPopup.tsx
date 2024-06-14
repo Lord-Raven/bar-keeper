e9825013bf6a720836788b28f837fe28112cbc83
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-14 09:02:03 -0400

    Messing with layout.

--- src/ImageWithPopup.tsx
+++ src/ImageWithPopup.tsx
@@ -25,7 +25,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
     const open = Boolean(anchorEl);
 
     return (
-        <div style={{height: '10vh', display: 'flex', alignItems: 'center'}}>
+        <div style={{height: '100%', display: 'flex', alignItems: 'center'}}>
             <img
                 src={src}
                 alt={alt}
@@ -38,6 +38,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
             <Popover
                 id={`mouse-over-popover-${popupHeader}`}
                 sx={{
+                    height: '10vh',
                     pointerEvents: 'none'
                 }}
                 open={open}
@@ -56,7 +57,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
                 <Box component="section" sx={{p: 2, border: '1px dashed grey'}}>
                     {popupSrc && (
                         <div style={{height: '100%', float: 'left'}}>
-                            <img src={popupSrc} style={{maxHeight: '100%', width: 'auto', verticalAlign: 'center'}} alt={alt}/>
+                            <img src={popupSrc} style={{height: '100%', width: 'auto', verticalAlign: 'center'}} alt={alt}/>
                         </div>
                     )}
                     <div>
