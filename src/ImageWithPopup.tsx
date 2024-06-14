73c33020db1079084b540de66929dde88df0a478
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-13 21:55:26 -0400

    Messing with layout.

--- src/ImageWithPopup.tsx
+++ src/ImageWithPopup.tsx
@@ -25,7 +25,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
     const open = Boolean(anchorEl);
 
     return (
-        <div style={{height: '10vh', display: 'flex', alignItems: 'center'}}>
+        <div style={{maxHeight: '10vh', display: 'flex', alignItems: 'center'}}>
             <img
                 src={src}
                 alt={alt}
