6a89c7e4399bf4cd7b3ed3b2c559b8c6e9d46701
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-15 17:26:11 -0400

    Trying out Windup.

--- src/ImageWithPopup.tsx
+++ src/ImageWithPopup.tsx
@@ -25,7 +25,7 @@ const ImageWithPopup: React.FC<ImageWithPopupProps> = ({ src, alt, popupHeader, 
     const open = Boolean(anchorEl);
 
     return (
-        <div style={{height: '100%', display: 'flex', alignItems: 'center'}}>
+        <div style={{height: '100%', alignItems: 'center'}}>
             <img
                 src={src}
                 alt={alt}
