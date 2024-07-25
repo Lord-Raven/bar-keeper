83b94d24f20098dba69dc2885b1f20bb8fd83b2e
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-07-25 15:03:43 +0000

    Attempted service worker revisions.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,30 @@
 self.addEventListener('install', event => {
-    console.log('installing service worker');
-    event.waitUntil(
-      caches.open('image-cache').then(cache => {
-        return cache.addAll([]);
+  console.log('installing service worker');
+  event.waitUntil(
+    caches.open('image-cache').then(cache => {
+      return cache.addAll([]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.destination === 'image') {
+    console.log('service worker fielding request');
+    event.respondWith(
+      caches.match(event.request).then((cacheResponse) => {
+        if (cacheResponse) {
+          console.log('found cached');
+          return cacheResponse;
+        }
+        console.log('not cached');
+        return fetch(event.request).then((networkResponse) => {
+          // Cache the fetched image
+          caches.open('image-cache').then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
       })
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.destination === 'image') {
-      console.log('service worker fielding request');
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            console.log('found cached');
-            return cachedResponse;
-          }
-          console.log('not cached');
-          return fetch(event.request).then((networkResponse) => {
-            // Cache the fetched image
-            caches.open('image-cache').then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-            });
-            return networkResponse;
-          });
-        })
-      );
-    }
-  });
+  }
+});
