4e81f61d8269737549d3f48159b7db1ab81e7e7f
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-07-26 20:50:46 +0000

    Patron images.

--- src/MessageWindow.tsx
+++ src/MessageWindow.tsx
@@ -75,13 +75,13 @@ export const MessageWindow: FC<MessageWindowProps> = ({ advance, slice, subSlice
                     }
                 </div>
             </Box>
-            <div style={{overflow: 'auto', display: 'flex', alignItems: 'flex-end', zIndex: 2, position: 'relative'}}> 
+            <div style={{position: 'relative'}}> 
                 {slice()?.presentPatronIds.map(patronId => {
                         if (stage().patrons[patronId]) {
                             if (patronId.toLowerCase().includes(subSlice().speakerId?.toLowerCase() ?? 'nevereverever')) {
-                                return <div style={{position: 'relative'}}>Main3: {patronId}<img src={stage().patrons[patronId].imageUrl} style={{position: 'absolute', bottom: 0, height: '35vh', width: 'auto'}}/></div>;
+                                return <div style={{position: 'relative'}}>Main6: {patronId}<img src={stage().patrons[patronId].imageUrl} style={{position: 'absolute', bottom: 0, height: '35vh', width: 'auto'}}/></div>;
                             } else {
-                                return <div style={{position: 'absolute', bottom: '0px'}}>Other5: {patronId}<img src={stage().patrons[patronId].imageUrl} style={{height: '30vh', width: 'auto'}}/></div>;
+                                return <div style={{position: 'absolute', bottom: '0px'}}>Other6: {patronId}<img src={stage().patrons[patronId].imageUrl} style={{height: '30vh', width: 'auto'}}/></div>;
                             }
                         } else {
                             return <div></div>;
