4cbba1b480f5b6fc9409fd1d46bc0c7aea8cc804
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-11-10 14:49:32 +0000

    Debugging.

--- src/Director.tsx
+++ src/Director.tsx
@@ -115,7 +115,8 @@ export class Director {
     constructor() { }
 
     getPromptInstruction(stage: Stage, slice: Slice): string {
-        console.log(`playerName: ${stage.player}\npatronName: ${slice.selectedPatronId}`);
+        console.log(stage.patrons);
+        console.log(`playerName: ${stage.player.name}\npatronName: ${slice.selectedPatronId ? stage.patrons[slice.selectedPatronId] : 'no selectedPatronId'}`);
         return directionInstructions[slice.direction ?? Direction.IntroduceBar]({barDescription: stage.barDescription ?? '', playerName: stage.player.name ?? '', patronName: slice.selectedPatronId ? stage.patrons[slice.selectedPatronId].name : ''});
     }
 
