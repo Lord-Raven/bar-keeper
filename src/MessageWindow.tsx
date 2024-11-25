890d46b9ec9ea2d9b9519e5122b3e70ceb6aa41c
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-11-24 21:09:46 -0500

    Messing with character animation.

--- src/MessageWindow.tsx
+++ src/MessageWindow.tsx
@@ -77,7 +77,7 @@ const PatronImage: FC<PatronImageProps> = ({imgUrl, xPosition, isTalking}) => {
             initial='idle'
             animate={isTalking ? 'talking' : 'idle'}
             className='important-overflow-visible'
-            style={{position: 'absolute', bottom: '-35vh', maxHeight: '100%', width: 'auto', aspectRatio: '5 / 12', zIndex: 10}}>
+            style={{position: 'absolute', maxHeight: '100%', width: 'auto', aspectRatio: '5 / 12', zIndex: 10}}>
             <img src={imgUrl} className='important-overflow-visible' style={{position: 'relative', width: '100%', height: '100%'}} alt='Patron Image'/>
         </motion.div>
     );
