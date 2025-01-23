d07f941f928fff48c6fad3a490ca5fc35d102f2f
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-01-23 10:50:04 -0500

    Beverage count.

--- src/Beverage.tsx
+++ src/Beverage.tsx
@@ -15,31 +15,37 @@ export class Beverage {
 
     render(selected: () => boolean, count: () => number, onClick: (id: string) => void): ReactElement {
         return this.imageUrl !== '' ? (
-                <Box component="section" sx={{
-                        p: 1,
-                        borderRadius: 3,
-                        border: selected() ? '3px solid yellow' : '',
-                        boxSizing: 'border-box',
-                        backgroundColor: count() > 0 ? '#00000000' : '#66666633',
-                        '&:hover': {backgroundColor: count() > 0 ? '#FFFFFF11' : '#66666666'}
-                }} onClick={() => {onClick(this.name);}}>
-                    <Badge
-                        badgeContent={`${count()}`}
-                        color="primary"
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'right',
-                        }}>
-                    </Badge>
-                    <ImageWithPopup
-                        src={this.imageUrl}
-                        alt={`${this.name}`}
-                        popupHeader={this.name}
-                        popupBody={this.description}
-                        popupSrc={this.imageUrl}
-                    />
-
-                </Box>) :
+                <Badge
+                    badgeContent={`${count()}`}
+                    color="primary"
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    sx={{
+                        '& .MuiBadge-badge': {
+                            backgroundColor: count() > 0 ? 'blue' : '#66666666',
+                            color: 'white',
+                            borderRadius: '50%',
+                        }
+                    }}>
+                    <Box component="section" sx={{
+                            p: 1,
+                            borderRadius: 3,
+                            border: selected() ? '3px solid yellow' : '',
+                            boxSizing: 'border-box',
+                            backgroundColor: count() > 0 ? '#00000000' : '#66666633',
+                            '&:hover': {backgroundColor: count() > 0 ? '#FFFFFF11' : '#66666666'}
+                    }} onClick={() => {onClick(this.name);}}>
+                        <ImageWithPopup
+                            src={this.imageUrl}
+                            alt={`${this.name}`}
+                            popupHeader={this.name}
+                            popupBody={this.description}
+                            popupSrc={this.imageUrl}
+                        />
+                    </Box>
+                </Badge>) :
                 <></>;
     }
 }
