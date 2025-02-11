94abb73023e6aa44ab10b34697b7b5b83fe134eb
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-02-11 18:30:56 -0500

    Tweaks to title screen stuff.

--- src/TitleScreen.tsx
+++ src/TitleScreen.tsx
@@ -25,10 +25,9 @@ export const TitleScreen: FC<TitleScreenProps> = ({ stage, setOnMenu }) => {
 
     return (
         <div style={{background: `radial-gradient(ellipse at center, #00000033 50%, #000000BB 90%)`, height: '100vh', width: '100vw'}}>
-            <div style={{backgroundImage: `url(${stage().titleUrl})`, backgroundPosition: 'top center', backgroundSize: 'auto 100%', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed',
-                    display: 'flex', justifyContent: 'center', flexDirection: 'column', height: '100vh', width: '100vw'}}>
+            <div style={{backgroundImage: `url(${stage().titleUrl})`, backgroundPosition: 'top center', backgroundSize: 'auto 100%', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', height: '100vh', width: '100vw'}}>
                 {generating ? (
-                    <div>
+                    <div style={{display: 'flex', flexDirection: 'column', bottom: '2vh', gap: '2vh', alignItems: 'center', marginTop: 'auto'}}>
                         <Box style={{backgroundColor: '#00000088'}} color={'primary'}>
                         <Typography>
                             {stage().loadingProgress}% - {stage().loadingDescription}
