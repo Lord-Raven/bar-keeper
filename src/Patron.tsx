fd686e0bd7e4b097287cdadae127e809b005fc19
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-04-18 14:29:44 -0400

    Logging for nameCheck.

--- src/Patron.tsx
+++ src/Patron.tsx
@@ -17,8 +17,9 @@ export class Patron {
 
 // Use Levenshtein distance to determine if an input string is referring to this character's name
 export function nameCheck(name: string, possibleName: string): boolean {
+    name = name.toLowerCase();
+    possibleName = possibleName.toLowerCase();
     const matrix = Array.from({ length: name.length + 1 }, () => Array(possibleName.length + 1).fill(0));
-
     for (let i = 0; i <= name.length; i++) {
         for (let j = 0; j <= possibleName.length; j++) {
             if (i === 0) {
@@ -35,7 +36,7 @@ export function nameCheck(name: string, possibleName: string): boolean {
         }
     }
     console.log(`Levenshtein distance between ${name} and ${possibleName}: ${matrix[name.length][possibleName.length]}`);
-    return matrix[name.length][possibleName.length] < Math.min(name.length / 2, possibleName.length / 2);
+    return matrix[name.length][possibleName.length] < Math.min(name.length / 1.5, possibleName.length / 1.5);
 }
 
 export enum Emotion {
