da5b15ac5b8e71581f3b555aeef6c1f1bc14aea5
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-10-04 23:51:13 -0400

    Debugging a message issue.

--- src/MessageWindup.tsx
+++ src/MessageWindup.tsx
@@ -36,8 +36,7 @@ function MessageWindup({message, read, options}: MessageWindupProps) {
             <div style={{position: 'absolute', top: '0px', left: '0px', zIndex: 10}}>
                 <WindupChildren key={message} {...options}>
                     <Pace ms={3}>
-                        {/*Old approach: <Typography>{TextWithQuotes({text: message, read})}</Typography> */}
-                        <Typography style={{display: 'inline-block'}}><TextWithQuotes text={message} read={read}/></Typography>
+                        <Typography><TextWithQuotes text={message} read={read}/></Typography>
                     </Pace>
                 </WindupChildren>
             </div>
